939fd73bd5abfbc0b099f513fa85747ec814b2e0
Alexandre Jacquot <ajacquot@pictet-technologies.com>
2021-05-01 16:16:30 +0200

    size limit for the description field

--- todo-list-application/src/main/java/com/pictet/technologies/opensource/reactive/r2dbc/todolist/rest/api/ItemUpdateResource.java
+++ todo-list-application/src/main/java/com/pictet/technologies/opensource/reactive/r2dbc/todolist/rest/api/ItemUpdateResource.java
@@ -6,12 +6,14 @@ import lombok.experimental.Accessors;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @Accessors(chain = true)
 public class ItemUpdateResource {
 
     @NotBlank
+    @Size(max=4000)
     private String description;
 
     @NotNull
--- todo-list-application/src/main/java/com/pictet/technologies/opensource/reactive/r2dbc/todolist/rest/api/NewItemResource.java
+++ todo-list-application/src/main/java/com/pictet/technologies/opensource/reactive/r2dbc/todolist/rest/api/NewItemResource.java
@@ -4,12 +4,14 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @Accessors(chain = true)
 public class NewItemResource {
 
     @NotBlank
+    @Size(max=4000)
     private String description;
 
 }
