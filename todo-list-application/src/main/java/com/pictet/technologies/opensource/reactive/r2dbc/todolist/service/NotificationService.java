4a76a868e2cfacc8a972e004875420aee559ff1c
Alexandre Jacquot <77357704+alexandre-jacquot-ptl@users.noreply.github.com>
2021-05-02 22:56:04 +0200

    Update NotificationService.java

--- todo-list-application/src/main/java/com/pictet/technologies/opensource/reactive/r2dbc/todolist/service/NotificationService.java
+++ todo-list-application/src/main/java/com/pictet/technologies/opensource/reactive/r2dbc/todolist/service/NotificationService.java
@@ -96,7 +96,8 @@ public class NotificationService {
      * @param topic Name of the topic to listen to
      */
     private void executeListenStatement(NotificationTopic topic) {
-        // Topic in upper-case must be surround by quotes
+        
+        // Topic in upper-case must be surrounded by quotes
         getConnection().createStatement(String.format("LISTEN \"%s\"", topic)).execute().subscribe();
     }
 
@@ -106,7 +107,8 @@ public class NotificationService {
      * @param topic Name of the topic to unlisten from
      */
     private void executeUnlistenStatement(NotificationTopic topic) {
-        // Topic in upper-case must be surround by quotes
+        
+        // Topic in upper-case must be surrounded by quotes
         getConnection().createStatement(String.format("UNLISTEN \"%s\"", topic)).execute().subscribe();
     }
 
@@ -138,6 +140,7 @@ public class NotificationService {
      * @return the object mapper
      */
     private ObjectMapper createObjectMapper() {
+       
         return new ObjectMapper()
                 .registerModule(new JavaTimeModule())
                 // This strategy is needed to match the DB column names with the entity field names
