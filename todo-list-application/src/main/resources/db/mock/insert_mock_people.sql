e2e237c011a55c518c95fe24f4134327e46199f8
Alexandre Jacquot <77357704+alexandre-jacquot-ptl@users.noreply.github.com>
2021-05-02 18:56:45 +0200

    Update insert_mock_people.sql

--- todo-list-application/src/main/resources/db/mock/insert_mock_people.sql
+++ todo-list-application/src/main/resources/db/mock/insert_mock_people.sql
@@ -1,6 +1,6 @@
 INSERT INTO person (version, first_name, last_name)
 values
-(1, 'Ricardo', 'Almighty'),
+(1, 'Richard', 'Countin'),
 (1, 'Nathalie', 'Queen'),
-(1, 'Vincent', 'Power');
+(1, 'Vince', 'Power');
 
