488d5ed9a08ac1293d16ae61068af0f6eaee5a3b
Alexandre Jacquot <ajacquot@pictet-technologies.com>
2021-05-15 16:29:12 +0200

    change mock data

--- todo-list-application/src/main/resources/db/mock/insert_mock_items.sql
+++ todo-list-application/src/main/resources/db/mock/insert_mock_items.sql
@@ -3,4 +3,4 @@ values
 (1, 'Book a flight to JNB', 1, 'TODO'),
 (2, 'Take the meeting minutes', 2, 'TODO'),
 (3, 'Organize a drink with the team', 3, 'IN_PROGRESS'),
-(4, 'Meet Mrs. Fox', 4, 'DONE');
+(4, 'Meet Mrs. Cosmic', 4, 'DONE');
