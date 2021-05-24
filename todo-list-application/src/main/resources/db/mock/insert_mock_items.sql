8def3c674e4f84f5ec644e9aaacb7ae1c9e60b74
Alexandre Jacquot <ajacquot@pictet-technologies.com>
2021-05-24 13:58:48 +0200

    update data

--- todo-list-application/src/main/resources/db/mock/insert_mock_items.sql
+++ todo-list-application/src/main/resources/db/mock/insert_mock_items.sql
@@ -1,6 +1,6 @@
 INSERT INTO ITEM (id, description, assignee_id, status)
 values
-(1, 'Book a flight to JNB', 1, 'TODO'),
-(2, 'Take the minutes', 2, 'TODO'),
-(3, 'Organize a drink with the team', 3, 'IN_PROGRESS'),
+(1, 'Flight to JNB', 1, 'TODO'),
+(2, 'Organise an celebration for the Great Place to Work', 2, 'TODO'),
+(3, 'Organise a drink with the team', 3, 'IN_PROGRESS'),
 (4, 'Meet Ms Cosmic', 4, 'DONE');
