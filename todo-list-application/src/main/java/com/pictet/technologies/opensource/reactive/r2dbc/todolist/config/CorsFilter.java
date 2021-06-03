8699b0df5e4b6761d84e93e79f9bcd70082bfbcd
Alexandre Jacquot <ajacquot@pictet-technologies.com>
2021-06-03 16:23:08 +0200

    use constants

--- todo-list-application/src/main/java/com/pictet/technologies/opensource/reactive/r2dbc/todolist/config/CorsFilter.java
+++ todo-list-application/src/main/java/com/pictet/technologies/opensource/reactive/r2dbc/todolist/config/CorsFilter.java
@@ -9,18 +9,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.springframework.http.HttpHeaders.*;
+
 @Component
 public class CorsFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
                                     final FilterChain filterChain) throws ServletException, IOException {
-        response.addHeader("Access-Control-Allow-Origin", "*");
-        response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD");
-        response.addHeader("Access-Control-Allow-Headers", "*");
-        response.addHeader("Access-Control-Expose-Headers", "*");
-        response.addHeader("Access-Control-Allow-Credentials", "true");
-        response.addIntHeader("Access-Control-Max-Age", 1);
+        response.addHeader(ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        response.addHeader(ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, DELETE, PUT, PATCH, HEAD");
+        response.addHeader(ACCESS_CONTROL_ALLOW_HEADERS, "*");
+        response.addHeader(ACCESS_CONTROL_EXPOSE_HEADERS, "*");
+        response.addHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+        response.addIntHeader(ACCESS_CONTROL_MAX_AGE, 1);
         filterChain.doFilter(request, response);
     }
 }
